07160e495aa3403a771456d0a72c5aed34e3d8d8
elevenandahalfjournal <elevenandahalfjournal@gmail.com>
2026-02-20 16:39:42 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import { defineConfig } from 'astro/config';
+
 export default defineConfig({
   site: 'https://elevenandahalfjournal.github.io',
+  base: '/digital-archive',
 });
